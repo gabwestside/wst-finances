da43900026812afb55f5fc4362601b47ce80f9c6
gabwestside <gabrielnfl13@gmail.com>
2021-09-02 13:07:24 -0300

    first commit

--- westsidefinances/src/components/Header/index.tsx
+++ westsidefinances/src/components/Header/index.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import Logo from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState();
+
+    //Sempre que uma função começa com handle, sabemos que o usuario irá clicar em algo!
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>

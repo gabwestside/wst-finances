8e304f03c1fb77a001dae5d63b030408d8d1aca7
gabwestside <gabrielnfl13@gmail.com>
2021-09-03 18:56:48 -0300

    first commit

--- westsidefinances/src/App.tsx
+++ westsidefinances/src/App.tsx
@@ -1,12 +1,36 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  //Sempre que uma função começa com handle, sabemos que o usuario irá clicar em algo!
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionNewModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+          >
+          <h2>Cadastrar Informação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- westsidefinances/src/components/Header/index.tsx
+++ westsidefinances/src/components/Header/index.tsx
@@ -1,24 +1,17 @@
-import { useState } from 'react';
 import Logo from '../../assets/logo.svg'
+
 import { Container, Content } from './styles'
 
-export function Header() {
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState();
+interface HeaderProps {
+    onOpenNewTransactionNewModal: () => void;
+}
 
-    //Sempre que uma função começa com handle, sabemos que o usuario irá clicar em algo!
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true);
-    }
-
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false);
-    }
-
+export function Header({ onOpenNewTransactionNewModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={Logo} alt="wstfncs" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionNewModal}>
                     Nova transação
                 </button>
             </Content>
